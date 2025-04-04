bb545b4d4e30dda612bdf490e8b30ff7cb69f7a4
Lardoo <kemboibethwel2@gmail.com>
2025-04-04 16:14:41 +0300

    commit to add css admin

--- vulnScanner/settings.py
+++ vulnScanner/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -183,6 +184,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Directory where collectstatic will place static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure Django uses WhiteNoise for serving static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
